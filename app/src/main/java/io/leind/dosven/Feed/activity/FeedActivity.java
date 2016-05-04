39c7bf9e743cc05f242921a9400f172464efd5d0
marcelarmz <danielcardeenas@gmail.com>
2016-05-04 17:59:30 -0500

    Added edit button to appbar

--- app/src/main/java/io/leind/dosven/Feed/activity/FeedActivity.java
+++ app/src/main/java/io/leind/dosven/Feed/activity/FeedActivity.java
@@ -7,14 +7,23 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.mikepenz.iconics.context.IconicsContextWrapper;
 
+import java.util.Arrays;
+import java.util.List;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import co.lujun.androidtagview.TagContainerLayout;
 import io.leind.dosven.Home.activity.BaseActivity;
 import io.leind.dosven.R;
 
 public class FeedActivity extends BaseActivity {
     @Bind(R.id.appbar)
     AppBarLayout appBar;
+
+    //@Bind(R.id.tag_container)
+    //TagContainerLayout tagContainer;
+
+    List<String> tags = Arrays.asList("sup1", "sup2", "sup3");
 
     /**
      * Font icons wrapper
@@ -30,6 +39,8 @@ public class FeedActivity extends BaseActivity {
         setContentView(R.layout.activity_feed);
         ButterKnife.bind(this);
         setTitle("");
+
+        //tagContainer.setTags(tags);
 
     }
 
